94353be87aeffb4c572dde2cbe0a7bfa19d38c46
NataliBubnova <Natali030215@mail.ru>
2021-11-12 19:23:41 +0300

    Bubnova Natalia

--- task_4/src/Task_4.java
+++ task_4/src/Task_4.java
@@ -1,91 +1,93 @@
-//
-// Source code recreated from a .class file by IntelliJ IDEA
-// (powered by FernFlower decompiler)
-//
-
 import java.util.Arrays;
 
 public class Task_4 implements Task_4_base {
-    public Task_4() {
-    }
-
+    @Override
     public int[] subtask_1_arrays(int size, int a0, int d) {
-        int[] intAr = new int[size];
-        intAr[0] = a0;
-
-        for(int i = 1; i < size; ++i) {
-            intAr[i] = intAr[i - 1] + d;
+        // сгенерировать и вернуть массив размера size, содержащий элементы
+        // арифметической прогрессии с первым членом a0 и разностью d
+        int a[] = new int [size];
+        a[0]=a0;
+        for (int i = 1; i < size; i++){
+            a[i] = a[i-1]+d;
         }
-
-        return intAr;
+        return a;
     }
 
+    @Override
     public int[] subtask_2_arrays(int size) {
-        int[] intAr = new int[size];
-        intAr[0] = 1;
+        // сгенерировать и вернуть массив размера size, первые два элемента
+        // которого равны единице, а каждый следующий - сумме всех предыдущих
+        int a[] = new int [size];
+        a[0]= 1;
         if (size > 1) {
-            intAr[1] = 1;
-
-            for(int i = 2; i < size; ++i) {
-                intAr[i] = Arrays.stream(intAr).sum();
+            a[1] = 1;
+            int b = a[0] + a[1];
+            for (int i = 2; i < size; i++, b = b+b ){
+                a[i] = b;
             }
         }
-
-        return intAr;
+        return a;
     }
 
+    @Override
     public int[] subtask_3_arrays(int size) {
-        int[] intAr = new int[size];
-        intAr[0] = 0;
+        // сгенерировать и вернуть массив размера size, содержащий первые
+        // size чисел последовательности фибоначчи.
+        // https://ru.wikipedia.org/wiki/Числа_Фибоначчи
+        int a [] = new int [size];
+        a[0] = 0;
         if (size > 1) {
-            intAr[1] = 1;
-
-            for(int i = 2; i < size; ++i) {
-                intAr[i] = intAr[i - 1] + intAr[i - 2];
+            a[1] = 1;
+            for ( int i = 2; i < size; i++) {
+                a[i] = a[i-1] + a[i-2];
             }
         }
-
-        return intAr;
+        return a;
     }
+
+    @Override
     public int subtask_4_arrays(int[] data) {
-        int max = data[0];
-
-        for(int i = 1; i < data.length; ++i) {
-            if (data[i] > max) {
+        // Для данного массива вычислить максимальный элемент
+        int max = data [0];
+        for (int i = 0; i < data.length; i++){
+            if (data[i] > max){
                 max = data[i];
             }
         }
-
         return max;
     }
 
+    @Override
     public int subtask_5_arrays(int[] data, int k) {
-        int max = -5;
-
-        for(int i = 1; i < data.length; ++i) {
-            if (data[i] > max && data[i] % k == 0) {
-                max = data[i];
+        // Для данного массива вычислить максимальный элемент
+        // кратный k. Гарантируется, что как минумум один такой элемент
+        // в массиве есть
+        int a = -999999;
+        for (int i = 1; i < data.length; i++){
+            if ((data[i]%k == 0) && (data [i]>a)) {
+                a = data[i];
             }
         }
-
-        return max;
+        return a;
     }
 
+    @Override
     public int[] subtask_6_arrays(int[] arr1, int[] arr2) {
-        int[] a = new int[arr1.length + arr2.length];
-        int k = 0;
-
-        int j;
-        for(j = 0; j < arr1.length; ++j) {
-            a[j] = arr1[j];
-            ++k;
+        // Даны два массива arr1, arr2.
+        // Произвести слияние данных массивов в один отсортированный
+        // по возрастанию массив.
+        int a[] = new int [arr1.length + arr2.length];
+        int a1 = arr1.length;
+        int a2 = arr2.length;
+        int j =0;
+        for (int i = 0; i < a1; i++) {
+            a[i] = arr1[i];
+            j++;
         }
-
-        for(j = 0; j < arr2.length; ++j) {
-            a[k] = arr2[j];
-            ++k;
+        for (int k = 0; k < a2; k++) {
+            a[j] = arr2[k];
+            j++;
         }
-
         Arrays.sort(a);
         return a;
     }
