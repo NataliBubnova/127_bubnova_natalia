edebe3c479c697dd88e053f426e33cdb9711cfbb
NataliBubnova <Natali030215@mail.ru>
2022-03-30 00:38:33 +0400

    Исправила ошибки и добавила в отчёт

--- 2sem/task_3/127_bubnova_task_3/files/Task3_Tests.java
+++ 2sem/task_3/127_bubnova_task_3/files/Task3_Tests.java
@@ -1,4 +1,95 @@
 import org.junit.*;
 public class Task3_Tests extends Assert {
-    
+    @Test
+    public void heapSort_GenerateNewHeapSort_CompareArrays(){
+        HeapSort<Integer> sorter = new HeapSort<>();
+        Integer[] array = {1};
+        MyComparator<Integer> comparator = new MyComparator<Integer>() {
+            @Override
+            public int compare(Integer first, Integer second) {
+                return first - second;
+            }
+        };
+        sorter.sort(array, comparator);
+        assertArrayEquals(new Integer[]{1}, array);
+    }
+    @Test
+    public void quickSort_GenerateQuickSort_CompareArrays(){
+        QuickSort<Integer> sorter = new QuickSort<>();
+        Integer[] array = {1};
+        MyComparator<Integer> comparator = new MyComparator<Integer>() {
+            @Override
+            public int compare(Integer first, Integer second) {
+                return first - second;
+            }
+        };
+        sorter.sort(array, comparator);
+        assertArrayEquals(new Integer[]{1}, array);
+    }
+    @Test
+    public void quickSort_GenerateQuickSort_CompareArraysTwoPositions(){
+        QuickSort<Integer> sorter = new QuickSort<>();
+        Integer[] array = {3, 4};
+        MyComparator<Integer> comparator = new MyComparator<Integer>() {
+            @Override
+            public int compare(Integer first, Integer second) {
+                return first - second;
+            }
+        };
+        sorter.sort(array, comparator);
+        assertArrayEquals(new Integer[]{3, 4}, array);
+    }
+    @Test
+    public void quickSort_GenerateQuickSort_CompareArraysThreePositions(){
+        QuickSort<Integer> sorter = new QuickSort<>();
+        Integer[] array = {3, 4, 5};
+        MyComparator<Integer> comparator = new MyComparator<Integer>() {
+            @Override
+            public int compare(Integer first, Integer second) {
+                return first - second;
+            }
+        };
+        sorter.sort(array, comparator);
+        assertArrayEquals(new Integer[]{3, 4, 5}, array);
+    }
+    @Test
+    public void quickSort_GenerateQuickSort_SortTwoPositions(){
+        QuickSort<Integer> sorter = new QuickSort<>();
+        Integer[] array = {2, 1};
+        MyComparator<Integer> comparator = new MyComparator<Integer>() {
+            @Override
+            public int compare(Integer first, Integer second) {
+                return first - second;
+            }
+        };
+        sorter.sort(array, comparator);
+        assertArrayEquals(new Integer[]{1, 2}, array);
+    }
+    @Test
+    public void quickSort_GenerateQuickSort_SortThreePositions(){
+        QuickSort<Integer> sorter = new QuickSort<>();
+        Integer[] array = {2, 1, 3};
+        MyComparator<Integer> comparator = new MyComparator<Integer>() {
+            @Override
+            public int compare(Integer first, Integer second) {
+                return first - second;
+            }
+        };
+        sorter.sort(array, comparator);
+        assertArrayEquals(new Integer[]{1, 2, 3}, array);
+    }
+    @Test
+    public void quickSort_GenerateQuickSort_SortFourReversedPositions(){
+        QuickSort<Integer> sorter = new QuickSort<>();
+        Integer[] array = {4, 3, 2, 1};
+        MyComparator<Integer> comparator = new MyComparator<Integer>() {
+            @Override
+            public int compare(Integer first, Integer second) {
+                return first - second;
+            }
+        };
+        sorter.sort(array, comparator);
+        assertArrayEquals(new Integer[]{1, 2, 3, 4}, array);
+    }
+
 }
