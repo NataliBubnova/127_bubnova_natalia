c568e1f18b6f4d66b78642cceffeecf4dbbfc856
NataliBubnova <Natali030215@mail.ru>
2022-03-29 23:44:34 +0400

    Исправила ошибки и добавила в отчёт

--- 2sem/task_2/127_bubnova_task_2/files/Task2_Tests.java
+++ 2sem/task_2/127_bubnova_task_2/files/Task2_Tests.java
@@ -25,10 +25,37 @@ public class Task2_Tests extends Assert {
     @Test
     public void bubbleSort_GenerateMyBubbleSort_TryToSortVariousNumbers(){
         BubbleSort<Integer> sorter = new BubbleSort<>();
-        Integer[] myArray = {2, 1, 3};
+        Integer[] myArray = {3, 1, 2};
         sorter.sort(myArray, new SomeComparator());
         assertArrayEquals(new Integer[]{1, 2, 3}, myArray);
     }
-
+    @Test
+    public void bubbleSort_GenerateBubbleSort_ToSortLongerArrayWithVariousNumbers(){
+        BubbleSort<Integer> sorter = new BubbleSort<>();
+        Integer[] myArray = {3, 2, 1, 7, 5};
+        sorter.sort(myArray, new SomeComparator());
+        assertArrayEquals(new Integer[]{1, 2, 3, 5, 7}, myArray);
+    }
+    @Test
+    public void bubbleSort_GenerateBubbleSort_CompareSameArrays(){
+        BubbleSort<Integer> sorter = new BubbleSort<>();
+        Integer[] myArray = {1, 1, 1, 1};
+        sorter.sort(myArray, new SomeComparator());
+        assertArrayEquals(new Integer[]{1, 1, 1, 1}, myArray);
+    }
+    @Test
+    public void bubbleSort_GenerateBubbleSort_CompareSmallArrays(){
+        BubbleSort<Integer> sorter = new BubbleSort<>();
+        Integer[] myArray = {2};
+        sorter.sort(myArray, new SomeComparator());
+        assertArrayEquals(new Integer[]{2}, myArray);
+    }
+    @Test
+    public void insertionSort_GenerateInsertionSort_CompareSameArrays(){
+        InsertionSort<Integer> sorter = new InsertionSort<>();
+        Integer[] myArray = {1, 2, 3};
+        sorter.sort(myArray, new SomeComparator());
+        assertArrayEquals(new Integer[]{1, 2, 3}, myArray);
+    }
 }
 
