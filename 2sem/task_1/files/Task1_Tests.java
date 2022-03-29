979961745e6661e72947feb44effe0dfa61d3f1a
NataliBubnova <Natali030215@mail.ru>
2022-03-29 17:21:11 +0400

    Проверила createDoubleLinkedList_GenerateEmpty_EmptyFalse

--- 2sem/task_1/files/Task1_Tests.java
+++ 2sem/task_1/files/Task1_Tests.java
@@ -1,4 +1,14 @@
 import org.junit.*;
 public class Task1_Tests extends Assert{
-
+    @Test
+    public void createDoubleLinkedList_GenerateEmpty_EmptyTrue(){
+        DoubleLinkedList<Integer> list = new DoubleLinkedList<>();
+        assertTrue(list.isEmpty());
+    }
+    @Test
+    public void createDoubleLinkedList_GenerateEmpty_EmptyFalse(){
+        DoubleLinkedList<Integer> list = new DoubleLinkedList<>();
+        list.pushBack(1);
+        assertFalse(list.isEmpty());
+    }
 }
