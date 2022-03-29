138736fc604afc0f1f15fde8ab2a4d390c4aa9e8
NataliBubnova <Natali030215@mail.ru>
2022-03-29 23:31:28 +0400

    исправила ошибки и добавила в отчёт

--- 2sem/task_2/127_bubnova_task_2/files/Task2_Tests.java
+++ 2sem/task_2/127_bubnova_task_2/files/Task2_Tests.java
@@ -1,2 +1,34 @@
+import org.junit.*;
+public class Task2_Tests extends Assert {
+    @Test
+    public void bubbleSort_ArrayContainsOnlyNumbersOne_ArrayHasNotChanged (){
+        BubbleSort<Integer> sorter = new BubbleSort<>();
 
+        MyComparator comparator = new MyComparator() {
+            @Override
+            public int compare(Object first, Object second) {
+                return 0;
+            }
+        };
+
+        Integer[] myArray = {1,2,3};
+        sorter.sort(myArray, comparator);
+        assertArrayEquals(new Integer[]{1, 2, 3}, myArray);
+    }
+
+    class SomeComparator implements MyComparator<Integer>{
+        @Override
+        public int compare(Integer first, Integer second) {
+            return first - second;
+        }
+    }
+    @Test
+    public void bubbleSort_GenerateMyBubbleSort_TryToSortVariousNumbers(){
+        BubbleSort<Integer> sorter = new BubbleSort<>();
+        Integer[] myArray = {2, 1, 3};
+        sorter.sort(myArray, new SomeComparator());
+        assertArrayEquals(new Integer[]{1, 2, 3}, myArray);
+    }
+
+}
 
